c3cd902628ab865661cf171eec7eaaf3ae773634
kanellam <sdi2000064@di.uoa.gr>
2022-12-23 16:50:23 +0200

    Commit

--- Assignment_1/hp/examples/hp_main.c
+++ Assignment_1/hp/examples/hp_main.c
@@ -11,30 +11,33 @@
 #define CALL_OR_DIE(call)     \
   {                           \
     BF_ErrorCode code = call; \
-    if (code != BF_OK) {      \
+    if (code != BF_OK)        \
+    {                         \
       BF_PrintError(code);    \
       exit(code);             \
     }                         \
   }
 
-int main() {
-  BF_Init(LRU);
+int main()
+{
+  CALL_BF(BF_Init(LRU));
 
   HP_CreateFile(FILE_NAME);
-  HP_info* info = HP_OpenFile(FILE_NAME);
+  HP_info *info = HP_OpenFile(FILE_NAME);
 
   Record record;
   srand(12569874);
   int r;
   printf("Insert Entries\n");
-  for (int id = 0; id < RECORDS_NUM; ++id) {
+  for (int id = 0; id < RECORDS_NUM; ++id)
+  {
     record = randomRecord();
     HP_InsertEntry(info, record);
   }
 
   printf("RUN PrintAllEntries\n");
   int id = rand() % RECORDS_NUM;
-  printf("\nSearching for: %d",id);
+  printf("\nSearching for: %d", id);
   HP_GetAllEntries(info, id);
 
   HP_CloseFile(info);
