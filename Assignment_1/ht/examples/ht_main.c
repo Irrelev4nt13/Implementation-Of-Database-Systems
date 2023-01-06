dfc477d0e63df5e85c969ff2be179a0ef332be58
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2023-01-06 02:41:08 +0200

    first commit

--- Assignment_1/ht/examples/ht_main.c
+++ Assignment_1/ht/examples/ht_main.c
@@ -5,7 +5,7 @@
 #include "bf.h"
 #include "ht_table.h"
 
-#define RECORDS_NUM 200 // you can change it if you want
+#define RECORDS_NUM 6 // you can change it if you want
 #define FILE_NAME "data.db"
 
 #define CALL_OR_DIE(call)     \
@@ -17,10 +17,16 @@
     }                         \
   }
 
+void print(HT_info* info) {
+  printf("number of blocks %ld\n", info->numBlocks);
+  for(int i=0;i<info->numBuckets;i++)
+    printf("Bucket %d: %ld\n", i, info->hashTable[i]);
+}
+
 int main() {
   BF_Init(LRU);
 
-  HT_CreateFile(FILE_NAME,10);
+  HT_CreateFile(FILE_NAME,1);
   HT_info* info = HT_OpenFile(FILE_NAME);
 
   Record record;
@@ -31,10 +37,10 @@ int main() {
     record = randomRecord();
     HT_InsertEntry(info, record);
   }
-
+  // print(info);
   printf("RUN PrintAllEntries\n");
   int id = rand() % RECORDS_NUM;
-  HT_GetAllEntries(info, &id);
+  HT_GetAllEntries(info, id);
 
   HT_CloseFile(info);
   BF_Close();
