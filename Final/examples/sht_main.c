b3fb2e5db1d6c5f32d188c19aeab8ec57815dc94
kanellam <sdi2000064@di.uoa.gr>
2023-01-08 11:02:35 +0200

    Commit

--- Final/examples/sht_main.c
+++ Final/examples/sht_main.c
@@ -47,7 +47,7 @@ int main()
   }
   // Τυπώνουμε όλες τις εγγραφές με όνομα searchName
   printf("RUN PrintAllEntries for name %s\n", searchName);
-  SHT_SecondaryGetAllEntries(info, index_info, searchName);
+  printf("%d blocks were traversed \n", SHT_SecondaryGetAllEntries(info, index_info, searchName));
   // Κλείνουμε το αρχείο κατακερματισμού και το δευτερεύον ευρετήριο
 
   SHT_CloseSecondaryIndex(index_info);
